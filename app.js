95713679c2533dc8f396c7361806a537105fe65a
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-18 20:59:05 -0400

    Added Express and handle GET method

--- app.js
+++ app.js
@@ -1,43 +1,12 @@
-//import library: http
-const http = require('http');
+//import library - express
+const express = require('express');
 
-//create server
-const server = http.createServer((req, res) => {
+const app = express();
 
-    if(req.url === '/favicon.ico'){
-        res.end();
-        return;
-    }
-
-    console.log('Incoming Request');
-    console.log(req.method, req.url);
-
-    if(req.method === 'POST'){
-
-        let body = '';
-
-        req.on('end', () => {
-            console.log(body);
-
-            //split the body
-            const userName = body.split('=')[1];
-
-            res.end('<h1>'+ userName +'</h1>');
-
-        });
-
-
-        req.on('data', (chunk) => {
-            body += chunk;
-        });
-    }else{
-        //send response response to the UI
-        //In which format- sending data to the UI  - HTML
-        //set content type
-        res.setHeader('Content-Type','text/html');
-        res.end('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>');
-    }
+//middleware to handle the request - GET
+app.use((req, res, next) => {
+    res.send('<h1>Hello Friends</h1>');
 });
 
-//set the port number to start the server
-server.listen(8080);
+
+app.listen(8080);
