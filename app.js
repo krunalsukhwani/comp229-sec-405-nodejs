bea041ca76fa5ae3209fcfc9392e99c121397a10
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-18 21:19:30 -0400

    Nodemon Library Example
    Install nodemon using below command:
    npm install nodemon --save-dev
    
    add command under scripts in package.json file

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 //import library - express
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-//middleware to handle the request - GET
+//middlware to handle the request when click on submit button
 app.use((req, res, next) => {
-    res.send('<h1>Hello Friends</h1>');
+    let body = '';
+
+    req.on('end', ()=>{
+        const userName = body.split("=")[1];
+        if(userName){
+            req.body = { name : userName };
+        }
+        next();
+    });
+
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
 });
 
+
+//middleware to handle the request GET
+app.use((req, res, next) => {
+
+  if(req.body){
+    return res.send('<h1> Welcome User : ' + req.body.name + '</h1>');
+  }  
+
+  res.send(
+    '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+  );
+});
 
 app.listen(8080);
